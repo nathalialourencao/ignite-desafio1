6e4e158c78e2012f8f5e41fe19ff73d232c7f1a8
Nathália Lourenção <nathalia.lourencao@fcamara.com.br>
2022-04-30 14:42:20 -0300

    :tada:

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,7 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    // Crie uma nova task com um id random, não permita criar caso o título seja vazio. 
     if (!newTaskTitle) return;
 
     const newTask = {
